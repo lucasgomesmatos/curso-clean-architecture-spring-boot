51579cc25ddd23f21a2bb591fc77281813ab87bb
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-04-22 23:11:53 -0300

    feat: infra

--- infrastructure/src/main/java/com/catalogo/infrastructure/Application.java
+++ infrastructure/src/main/java/com/catalogo/infrastructure/Application.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @OpenAPIDefinition(info = @Info(title = "API", version = "v1"))
 @SpringBootApplication(scanBasePackages = {"com.catalogo.*"})
 public class Application {
     public static void main(String[] args) {
+       // System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(Application.class, args);
     }
 }
